1e72215a4bf18ba8b4adf8a7cfd4b1aa9b10bb15
s24580 <92123980+s24580@users.noreply.github.com>
2022-01-26 01:10:31 +0100

    Add files via upload

--- proj_1/proj.cpp
+++ proj_1/proj.cpp
@@ -5,18 +5,22 @@
 #include "SFML/Window/Mouse.hpp"
 #include "SFML/System.hpp"
 #include "SFML/Graphics/Color.hpp"
-
+#include "SFML/Window/Keyboard.hpp"
+#include "SFML/System/Clock.hpp"
+#include "SFML/Graphics/Drawable.hpp"
  //em skopiowane z biblioteki sfml bo próbuje zrozumieć mouse button
  #define SFML_MOUSE_HPP
+ 
     // Headers
    #include <SFML/Window/Export.hpp>
    #include <SFML/System/Vector2.hpp>
 
 int main () 
 {
-	std::srand (time(0)); //losowansko
-
-	sf::RenderWindow app(sf::VideoMode(400,400), "Sweep"); //render okna działa
+	std::srand (time(0)); //losowansko/git
+	
+	sf::RenderWindow app(sf::VideoMode(600,600), "Sweep"); //render okna działa
+	
 	
 	 int w=32;
 	int grid [12][12];
@@ -25,8 +29,12 @@ int main ()
 	sf::Texture txt;
 	txt.loadFromFile("kafle/tiles.jpg"); //wczyt jpg dla tekstur kafli
 	sf::Sprite s(txt);
-
-
+	
+	sf::Clock zegar;			// kurde nie da sie renderowac czasu do okna
+	sf::Time czas = zegar.getElapsedTime();
+	float i = czas.asSeconds();
+	
+	
 	for (int i=1;i<=10;i++)			//odswierzanie i tabelka
 		for (int j=1;j<=10;j++)
 			{
@@ -50,8 +58,10 @@ int main ()
         grid[i][j]=n;				
 	}
 
-	while (app.isOpen())	//wczytwyanie mysz
+	while (app.isOpen())	//wczytwyanie mysz/warning tylko
 	{
+	
+	
 	sf::Vector2i pos = sf::Mouse::getPosition(app);	
 	int x = pos.x/w;
 	int y = pos.y/w;
@@ -67,7 +77,7 @@ int main ()
 		}	
 	
 	app.clear(sf::Color::White);
-					//nie działa biała plansza jest przezroczysta
+					//nie działa biała plansza jest przezroczysta//naprawione
 	for (int i=1;i<=10;i++)		
 		for (int j=1;j<=10;j++)
 		{
@@ -75,16 +85,39 @@ int main ()
            	s.setTextureRect(sf::IntRect(sgrid[i][j]*w,0,w,w));
            	s.setPosition(i*w, j*w);
            	app.draw(s);
+           	/*else 
+           	#define _pelne_pol(wlas,i,j):
+           	tile = wlas.(sf::IntRect(grid[i][j])) 
+           	wlas._show_tile(pos.tile)
+           		if (tile.texture == s.[0]):
+           			neighbors = wlas._get_tile_neighbors(pos.tile)
+           				for i in neighbors:
+           					if (wlas.grid[i[0]][i[1]].ishidden):
+           						wlas._pelne_pol(i[0],i[1])*/
+           	
 		}
-		
+	
+	sf::Event quit;//quit pod esc
+	while (app.pollEvent(quit))
+	{
+	if (quit.type == sf::Event::KeyPressed)
+		{	
+		if (quit.key.code == sf::Keyboard::Escape)
+			{
+			app.close();
+			}
+		}
+	}
+	
+	/*app.draw(i);
+	if( i >= 1 )
+	{
+	zegar.restart();
+	}*/
+
 	app.display(); //pokazywanie
 	}
-	sf::Event quit;
-	while (window.pollEvent(quit))
-	{
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1);
-	window.close();
-	}
+	
 		
 	return 0;
 }
